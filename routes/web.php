4564d6e0798abcf9bb62a6134e8cc396f37b674a
Eryc <eryc.matos@liberfly.com.br>
2021-12-14 14:32:47 -0300

    Routes cadastro, route ver cadastro

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,33 @@
 <?php
 
+use App\Models\Cadastro;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('liberAcademy');
 });
+
+
+//Route of created of client
+Route::post('/cliente/store', function (Request $request) {
+    Cadastro::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => $request->password,
+        'phone' => $request->phone,
+        'cidade' => $request->cidade,
+    ]);
+    return "2 patinhos na lagoa";
+});
+
+Route::get('/cliente/show/{id}', function($id) {
+    return Cadastro::find($id);
+});
+
+//select all from 
+Route::get('/cliente/index', function() {
+    return Cadastro::all();
+});
+
