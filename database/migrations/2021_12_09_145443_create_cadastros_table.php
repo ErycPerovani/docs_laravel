9367b56e72e01055416c7145b0c3058948147324
Eryc <eryc.matos@liberfly.com.br>
2021-12-14 14:29:58 -0300

    create migration cadastro

---
+++ database/migrations/2021_12_09_145443_create_cadastros_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCadastrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // name: ok
+        // email: ok
+        // senha: ok
+        // telefone: ok
+        // cidade: ok
+
+
+        Schema::create('cadastros', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone');
+            $table->string('cidade');
+            $table->timestamps(); //created_at _|_ updated_at
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cadastros');
+    }
+}
