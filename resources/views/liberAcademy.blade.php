e69cdbd5e57af2a4d1d93fbe02e7cbdf44fd1a81
Eryc <eryc.matos@liberfly.com.br>
2021-12-14 14:40:04 -0300

    pagina de cadastro, direta no banco de dados

---
+++ resources/views/liberAcademy.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LiberAcademy</title>
+</head>
+<body>
+    <form action="/cadastrade" method="POST">
+    @csrf
+        <label>Nome: </label>
+        <input type="text" name="name" >
+        <br><br>
+        <label>E-mail: </label>
+        <input type="text" name="email">
+        <br><br>
+        <label>Senha: </label>
+        <input type="text" name="password" >
+        <br><br>
+        <label>Telefone: </label>
+        <input type="text" name="phone">
+        <br><br>
+        <label>Cidade: </label>
+        <input type="text" name="cidade">
+        <br><br>
+        <button>Cadastrar</button>
+    </form>
+    
+</body>
+</html>
