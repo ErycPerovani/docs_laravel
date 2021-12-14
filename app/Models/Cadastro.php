dbebdc6fe9163bcaef69f0b0f1422a21b9985f42
Eryc <eryc.matos@liberfly.com.br>
2021-12-14 14:38:02 -0300

    identifica com quais areas irei trabalhar

---
+++ app/Models/Cadastro.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cadastro extends Model
+{
+
+    //identifica com quais areas eu irei trabalhar.
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone',
+        'cidade'
+    ];
+}
